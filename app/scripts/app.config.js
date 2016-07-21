d871a479a11c913e00f46f68ee808f6b574ad411
Angelo Cisneros <a.e.cisneros@gmail.com>
2016-07-21 16:45:17 -0700

    Add ui-router, change editorConfig

---
+++ app/scripts/app.config.js
@@ -0,0 +1,13 @@
+angular.module('psJwtApp')
+.config(function ($urlRouterProvider, $stateProvider) {
+    $urlRouterProvider.otherwise('/');
+    $stateProvider
+        .state('main', {
+            url: '/',
+            templateUrl: '/views/main.html'
+        })
+        .state('register', {
+            url: '/register',
+            templateUrl: '/views/register.html'
+        });
+});
