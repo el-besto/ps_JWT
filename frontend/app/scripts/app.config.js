67fd3ed4db43ede1b35bab694ecc3247eaaf0134
Angelo Cisneros <a.e.cisneros@gmail.com>
2016-07-22 08:09:48 -0700

    Move clientside code into frontend folder

---
+++ frontend/app/scripts/app.config.js
@@ -0,0 +1,15 @@
+angular.module('psJwtApp')
+    .config(function ($urlRouterProvider, $stateProvider) {
+        'use strict';
+
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('main', {
+                url: '/',
+                templateUrl: '/views/main.html'
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: '/views/register.html'
+            });
+    });
