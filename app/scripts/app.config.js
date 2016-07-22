b211d3c5a90da05880b89de91fa4722a0c248541
Angelo Cisneros <a.e.cisneros@gmail.com>
2016-07-21 17:02:38 -0700

    ui-router config.

--- app/scripts/app.config.js
+++ app/scripts/app.config.js
@@ -1,13 +1,15 @@
 angular.module('psJwtApp')
-.config(function ($urlRouterProvider, $stateProvider) {
-    $urlRouterProvider.otherwise('/');
-    $stateProvider
-        .state('main', {
-            url: '/',
-            templateUrl: '/views/main.html'
-        })
-        .state('register', {
-            url: '/register',
-            templateUrl: '/views/register.html'
-        });
-});
+    .config(function ($urlRouterProvider, $stateProvider) {
+        'use strict';
+
+        $urlRouterProvider.otherwise('/');
+        $stateProvider
+            .state('main', {
+                url: '/',
+                templateUrl: '/views/main.html'
+            })
+            .state('register', {
+                url: '/register',
+                templateUrl: '/views/register.html'
+            });
+    });
