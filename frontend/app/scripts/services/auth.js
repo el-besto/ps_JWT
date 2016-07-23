783f509e8dee2ccc7bb69e7fe9881232bd410797
Angelo Cisneros <a.e.cisneros@gmail.com>
2016-07-23 14:33:46 -0700

    Add bare authService to FE

---
+++ frontend/app/scripts/services/auth.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc service
+ * @name psJwtApp.auth
+ * @description
+ * # auth
+ * Service in the psJwtApp.
+ */
+angular.module('psJwtApp')
+    .service('auth', function () {
+        // AngularJS will instantiate a singleton by calling "new" on this function
+    });
