9ad4da8db3018a000bd862a4ca151e8157d47df8
Angelo Cisneros <a.e.cisneros@gmail.com>
2016-07-22 17:08:11 -0700

    Move header to a View. Add in-line controller to hide register link if authToken is cached

---
+++ frontend/app/scripts/controllers/header.js
@@ -0,0 +1,13 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name psJwtApp.controller:HeaderController
+ * @description
+ * # HeaderController
+ * Controller of the psJwtApp
+ */
+angular.module('psJwtApp')
+    .controller('HeaderController', function ($scope, authToken) {
+        $scope.isAuthenticated = authToken.isAuthenticated();
+    });
