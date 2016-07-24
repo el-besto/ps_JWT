42c4ac0af27c2146e468c98030b208248668bbab
Angelo Cisneros <a.e.cisneros@gmail.com>
2016-07-23 17:56:25 -0700

    Oauth Google: Add `googleId` and `displayName` to User model

--- api/models/User.js
+++ api/models/User.js
@@ -3,7 +3,9 @@ var mongoose = require('mongoose');
 
 var UserSchema = new mongoose.Schema({
     username: String,
-    password: String
+    password: String,
+    googleId: String,
+    displayName: String
 });
 
 UserSchema.methods.comparePasswords = function (password, next) {
