1a2e3b9cbd7badfbc76d9fdf0bde249ea254d601
Angelo Cisneros <a.e.cisneros@gmail.com>
2016-07-24 17:06:00 -0700

    Base sails-rest-api install w/cipherService and Mailer options
    * Used a yeoman generator for sailsJS called sails-rest-api
    * See [sails-rest-api Wiki on Github](https://github.com/ghaiklor/generator-sails-rest-api/wiki)
    * See the project wikis section on [sub-generators](https://github.com/ghaiklor/generator-sails-rest-api/wiki/Sub-Generators) that should be used instead of default sailsJS generators

---
+++ sapi/config/services/cipher.js
@@ -0,0 +1,11 @@
+"use strict";
+
+module.exports = {
+  services: {
+    cipher: {
+      jwt: {
+        secretKey: '0f45b0faa21e97850f6fb94f55514a7f48068f9e3c59f09c20e3005946e45a2b'
+      }
+    }
+  }
+};
